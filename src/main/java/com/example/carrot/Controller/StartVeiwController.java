4e823bdeebab36d78494f24ea67ac335e38231e5
anstj <anstjsals4078@koreatech.ac.kr>
2024-02-10 10:48:09 +0900

    중고거래 - 사진 이상 동네업체 부동산 중고차 병합

--- src/main/java/com/example/carrot/Controller/StartVeiwController.java
+++ src/main/java/com/example/carrot/Controller/StartVeiwController.java
@@ -6,12 +6,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StartVeiwController {
 
-    @GetMapping("/locationCompany")
+    @GetMapping("")
     public String getstartForm(){
+        return "start";
+    }
+
+    @GetMapping("/locationCompany")
+    public String getlocationForm(){
         return "locationCo";
     }
+
     @GetMapping("/secondhand")
     public String getsecondhandForm(){
         return "secondhand";
     }
+
+    @GetMapping("/jobs")
+    public String getjobsForm(){
+        return "jobs";
+    }
+
+
+    @GetMapping("/realty")
+    public String getrealtyForm(){return "realty";}
+
+    @GetMapping("/car")
+    public String getcarForm(){
+        return "car";
+    }
+    @GetMapping("/qna")
+    public String getqnaForm(){ return "qna";}
 }
